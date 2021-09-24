33d4c720d0f9b4c6f0a445f85a1a9749dec27899
Dulsara <sandaminidulsara9896@gmail.com>
2021-09-25 00:51:22 +0530

    ui all created

---
+++ app/src/main/java/com/example/eguru_madproject/updatemyfeedbacks.java
@@ -0,0 +1,14 @@
+package com.example.eguru_madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class updatemyfeedbacks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updatemyfeedbacks);
+    }
+}
