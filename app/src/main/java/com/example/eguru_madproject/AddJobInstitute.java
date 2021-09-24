6f270957a6936bfadc8c2db5351b0ab7aec8863c
Hasini <hasinieshani@gmail.com>
2021-09-25 00:49:00 +0530

    edition

---
+++ app/src/main/java/com/example/eguru_madproject/AddJobInstitute.java
@@ -0,0 +1,14 @@
+package com.example.eguru_madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddJobInstitute extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_job_institute);
+    }
+}
