9540482083d887d474b56159bcf2cd9ec2374c63
Rajith Singh <rajithsingh2000@gmail.com>
2021-09-25 14:45:32 +0530

    get login UIs

--- app/src/main/java/com/example/eguru_madproject/signup.java
+++ app/src/main/java/com/example/eguru_madproject/signup.java
@@ -2,13 +2,35 @@ package com.example.eguru_madproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signup extends AppCompatActivity {
 
+    private TextInputEditText nameEdit, emailEdit, pawEdit, mobileEdit;
+    private Button registerBtn;
+    private TextView loginform;
+    private FirebaseAuth mAuth;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        nameEdit = findViewById(R.id.singup_name);
+        emailEdit = findViewById(R.id.signup_email);
+        pawEdit = findViewById(R.id.signup_password);
+        mobileEdit = findViewById(R.id.signup_mobile_num);
+        loginform = findViewById(R.id.signup_mobile_num);
+        registerBtn = findViewById(R.id.signup_btn);
+
+
+
     }
 }
